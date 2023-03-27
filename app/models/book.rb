a9cb05d4d090400f72b20d6629786a6e5b46e4a5
Fumiya2 <124821309+Fumiya2@users.noreply.github.com>
2023-03-27 15:59:38 +0000

    [Update]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+ has_one_attached :image
+ belong_to :user
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
